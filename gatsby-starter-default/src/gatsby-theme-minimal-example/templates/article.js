7b253c04196a11394291ecc9f82ec6d262c8f861
laradevitt <ldevitt@othermachines.com>
2019-12-06 13:16:01 -0700

    Wrong error message in comment.

--- gatsby-starter-default/src/gatsby-theme-minimal-example/templates/article.js
+++ gatsby-starter-default/src/gatsby-theme-minimal-example/templates/article.js
@@ -5,7 +5,7 @@ import Layout from "../../components/layout"
 const Article = ({ data }) => <Layout><pre>{JSON.stringify(data, null, 4)}</pre></Layout>
 
 // Changing the name of this query to 'ArticleQuery' will result in error:
-// "RelayParser: Encountered duplicate definitions for one or more documents".
+// `Multiple "root" queries found in file: "ArticleQuery" and "ArticleQuery"`
 export const query = graphql`
   query ArticleDupeQuery($uid: String!) {
     prismic {
